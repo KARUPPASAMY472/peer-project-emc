82505ba139a1e0c7cb0cfb53bd5a8543fbf4dbff
KARUPPASAMY M <mkaruppas477@gmail.com>
2025-11-23 08:32:33 +0530

    Backend update

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -40,3 +40,44 @@ router.delete("/", removeBookmark);
 router.get("/:uid", getBookmarksForUser);
 
 export default router;
+
+
+
+
+// import express from "express";
+// import { 
+//   addBookmark, 
+//   removeBookmark, 
+//   getBookmarksForUser 
+// } from "../controllers/bookmarkController.js";
+
+// const router = express.Router();
+
+// // Check bookmark
+// router.get("/check", async (req, res) => {
+//   try {
+//     const { userUid, projectId } = req.query;
+
+//     if (!userUid || !projectId) {
+//       return res.status(400).json({ bookmarked: false });
+//     }
+
+//     const exists = await Bookmark.findOne({ userUid, projectId });
+//     res.json({ bookmarked: !!exists });
+
+//   } catch (err) {
+//     console.error("Bookmark check error:", err);
+//     res.status(500).json({ bookmarked: false });
+//   }
+// });
+
+// // Add bookmark
+// router.post("/", addBookmark);
+
+// // Remove bookmark
+// router.delete("/", removeBookmark);
+
+// // Get bookmarks for a specific user
+// router.get("/:uid", getBookmarksForUser);
+
+// export default router;
